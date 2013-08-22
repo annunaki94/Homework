af5dd84ce7a3b2cb72e41cd5cbd439edb450ed1c
Cesar Villamizar <cavillamizarc@unal.edu.co>
2013-08-21 20:16:11 -0500

    Tratamiento de excepcion en Pyramid.java

--- 20-08-2013/Pyramid.java
+++ 20-08-2013/Pyramid.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 public class Pyramid{
 
 	public static void main(String args[]){
-		System.out.println("Ingrese la altura de la piramide");
-		Scanner sc= new Scanner(System.in);
-		int altura= sc.nextInt();		
+		System.out.println("Ingrese la altura de la piramide");		
+		boolean isInteger= false;
+		int altura=1;		
+		do{
+			try{
+				Scanner sc= new Scanner(System.in);
+				altura= sc.nextInt();
+				isInteger=true;			
+			}catch(Exception ex){
+				System.out.println("Número inválido, vuelva a intentarlo");
+				isInteger=false;
+			}
+		}while(!isInteger);		
+				
 		for(int i=0; i<altura;i++){
 			for(int k=i;k<altura-1;k++){ 
 				System.out.print(" ");}
